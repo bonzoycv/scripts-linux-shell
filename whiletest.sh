9867e6ab41df2397e9414242cdd435b7c2501e24
Yahir <bonzoycv@yandex.com>
2022-03-21 16:49:56 +0100

    script de ejemplo #1 while loop

--- whiletest.sh
+++ whiletest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Scrip de ejemplo while loop
+
 variable=1
 
 while [ $variable -le 10 ]
@@ -7,5 +9,5 @@ do
 
 	echo $variable
 	variable=$(($variable + 1))
-	sleep 1
+	sleep 0.5
 done
